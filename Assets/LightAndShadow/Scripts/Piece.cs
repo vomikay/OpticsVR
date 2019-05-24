9b01984e7d7f0600d19f386deb522e94c4730b1a
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-05-24 23:58:54 +0300

    add game mechanics in LightAndShadow

---
+++ Assets/LightAndShadow/Scripts/Piece.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+	public const float MOVEMENT_INCREMENT = 0.1f;
+
+    [SerializeField] private TargetPiece targetPiece = default;
+
+    public bool IsCorrect { get; private set; }
+
+    private void Start()
+    {
+        this.IsCorrect = false;
+    }
+
+    private void Update()
+	{
+		if (!this.IsCorrect)
+        {
+            this.CheckIsCorrect();
+        }
+	}
+
+    private void CheckIsCorrect()
+	{
+        if (targetPiece.CheckIsCorrect(this.transform))
+        {
+            this.IsCorrect = true;
+            StartCoroutine(this.LerpTowardTarget(targetPiece.transform));
+        }
+	}
+
+    private IEnumerator LerpTowardTarget(Transform targetTransform)
+	{
+		float t = 0f;
+		Vector3 startPosition = this.transform.position;
+
+		while (t < 1f)
+		{
+			this.transform.position = Vector3.Lerp(startPosition, targetTransform.position, t);
+			t += Time.deltaTime;
+			yield return null;
+		}
+	}
+
+}
---
+++ Assets/LightAndShadow/Scripts/TargetPiece.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPiece : MonoBehaviour
+{
+    public const float DISTANCE_COEFFICIENT = 10f;
+
+    [SerializeField] float positionTolerance = 1f;
+
+    public bool CheckIsCorrect(Transform pieceTransform)
+	{
+		float distance = Vector3.Distance(this.transform.position, pieceTransform.position);
+		return ((distance * DISTANCE_COEFFICIENT) < this.positionTolerance);
+	}
+}
