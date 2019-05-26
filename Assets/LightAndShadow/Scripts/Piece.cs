76ef84638bdfb148caca20036c1f7e3b671df406
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-05-27 00:29:35 +0300

    game object interaction in LightAndShadow scene

--- Assets/LightAndShadow/Scripts/Piece.cs
+++ Assets/LightAndShadow/Scripts/Piece.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
+[RequireComponent( typeof( Interactable ) )]
 public class Piece : MonoBehaviour
 {
 	public const float MOVEMENT_INCREMENT = 0.1f;
 
     [SerializeField] private TargetPiece targetPiece = default;
+    private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & ( ~Hand.AttachmentFlags.SnapOnAttach ) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+    private Interactable interactable;
 
     public bool IsCorrect { get; private set; }
 
     private void Start()
     {
         this.IsCorrect = false;
+        interactable = this.GetComponent<Interactable>();
     }
 
     private void Update()
@@ -28,6 +33,7 @@ public class Piece : MonoBehaviour
         if (targetPiece.CheckIsCorrect(this.transform))
         {
             this.IsCorrect = true;
+            Debug.Log("Correct Piece");
             StartCoroutine(this.LerpTowardTarget(targetPiece.transform));
         }
 	}
@@ -44,5 +50,22 @@ public class Piece : MonoBehaviour
 			yield return null;
 		}
 	}
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+        if (this.interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            hand.HoverLock(this.interactable);
+            hand.AttachObject(this.gameObject, startingGrabType, attachmentFlags);
+        }
+        else if (isGrabEnding)
+        {
+            hand.DetachObject(this.gameObject);
+            hand.HoverUnlock(this.interactable);
+        }
+    }
 
 }
